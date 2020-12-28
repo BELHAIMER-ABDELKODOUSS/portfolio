0e0423e097ed0297ae15e0045928adb459fe7518
Abdelkodouss Belhaimer <abdelkodoussbelhaimerbusiness@gmail.com>
2020-12-28 13:33:31 +0100

    lofi

--- Portfolio_Project/settings.py
+++ Portfolio_Project/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '7t!!lcjwkx+*pf3+#@d&*+x8z16!mdzh$r!j4f+s($^ob4-#8y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.0", "selfportfolio.herokuapp.com"]
 
