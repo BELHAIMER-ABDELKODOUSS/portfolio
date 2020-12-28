b50741151fdcc3db5e7519cb8fe3ea4cf29fb416
Abdelkodouss Belhaimer <abdelkodoussbelhaimerbusiness@gmail.com>
2020-12-28 15:39:38 +0100

    final

--- Portfolio_Project/settings.py
+++ Portfolio_Project/settings.py
@@ -47,7 +47,7 @@ ROOT_URLCONF = 'Portfolio_Project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
