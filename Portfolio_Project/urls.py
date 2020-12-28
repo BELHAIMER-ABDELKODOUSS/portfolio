db62c38f56e6c9830b274b1312b046cf871020f6
Abdelkodouss Belhaimer <abdelkodoussbelhaimerbusiness@gmail.com>
2020-12-28 13:50:03 +0100

    final

--- Portfolio_Project/urls.py
+++ Portfolio_Project/urls.py
@@ -8,7 +8,7 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('port_app.urls', namespace='Portfolio')),
+    path('', include('port_app.urls', namespace='Portfolio')),
     url(r'^media/(?P<path>.*)$', serve,
         {'document_root': settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve,
